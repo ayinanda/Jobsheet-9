ec3ec892e3f323930b5a8dad0efbe18dfdcbd558
ayinanda <ayiniswati22@gmail.com>
2025-05-02 14:54:54 +0700

    Percobaan 1 stack

---
+++ Mahasiswa09.java
@@ -0,0 +1,18 @@
+public class Mahasiswa09 {
+
+    String nama;
+    String nim;
+    String kelas;
+    int nilai;
+
+    Mahasiswa09(String nama, String nim, String kelas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+    }
+
+    void tugasDinilai (int nilai) {
+        this.nilai = nilai;
+    }
+}
+
